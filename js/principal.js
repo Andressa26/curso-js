53dfedd213a93b97b2e4867519888fd041c030b2
Ana <andressa.ro26@gmail.com>
2020-08-28 14:33:48 -0300

    Iniciando calculo de IMC dos pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,21 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso =  paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc")
+var imc = peso / (altura*altura);
+tdImc.textContent = imc;
+
+console.log(paciente);
+console.log(peso);
+console.log(tdPeso);
+console.log(tdAltura);
+console.log(altura);
+console.log(imc);
