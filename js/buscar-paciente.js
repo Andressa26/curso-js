6a4cdf9e555b47e2e4857b8b56c5fe46b9792984
Andressa Rodrigues <andressa.ro26@gmail.com>
2020-08-31 15:39:46 -0300

    Correção na url para requisição

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -2,7 +2,7 @@ var botaoBuscar = document.querySelector("#buscar-pacientes");
 
 botaoBuscar.addEventListener("click", function(){
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api-pacientes.herokuap.com/pacientes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     xhr.addEventListener("load", function(){
         console.log(xhr.responseText);
     });
