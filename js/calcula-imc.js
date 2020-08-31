109b0e74b16b9894bf4e286a016befd3b8b6bcdd
Andressa Rodrigues <andressa.ro26@gmail.com>
2020-08-31 09:22:11 -0300

    Validando peso do paciente antes do cadastro

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -12,15 +12,15 @@ for(i=0; i<pacientes.length; i++){
 
     var tdImc = paciente.querySelector(".info-imc")
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-    if(peso<=0 || peso>=1000){
+    if(!pesoEhValido){
         pesoEhValido = false;
         tdPeso.textContent = "Peso inválido!";
         //spaciente.style.backgroundColor = "lightcoral";
     }
-    if(altura<=0 || altura>=3.0){
+    if(!alturaEhValida){
         alturaEhValida = false;
         tdAltura.textContent = "Altura inválida!";
         
@@ -32,6 +32,24 @@ for(i=0; i<pacientes.length; i++){
     else {
         tdImc.textContent = "Altura e/ou peso inválidos!"
         paciente.classList.add("paciente-invalido")
+    }
+}
+
+function validaPeso(peso){
+    if(peso>= 0 && peso<1000){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura>= 0 && altura< 3.0){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
