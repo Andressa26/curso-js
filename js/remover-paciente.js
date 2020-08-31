f67f4fa0727ead238f1b3bf6a527bba333f61378
Andressa Rodrigues <andressa.ro26@gmail.com>
2020-08-31 11:19:03 -0300

    Removendo qualquer paciente da tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,9 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente) {
-    paciente.addEventListener("dblclick", function(){
-         console.log("fui clicado 2x");
-         this.remove();
-    });
-   
-});
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.addEventListener("dblclick", function(event){
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode;
+    paiDoAlvo.remove();
+    //event.target.parentNode.remove();
+})
