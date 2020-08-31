d01ab6fab6a067e5a4d16b92a38d985da3db12cb
Andressa Rodrigues <andressa.ro26@gmail.com>
2020-08-31 11:54:57 -0300

    Animando a remoção de pacientes

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,7 +3,11 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("#tabela-pacientes");
 tabela.addEventListener("dblclick", function(event){
     var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode;
-    paiDoAlvo.remove();
+    alvoEvento.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+         alvoEvento.parentNode.remove();
+    }, 500);
+  
     //event.target.parentNode.remove();
 })
